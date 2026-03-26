d1f3e674145835d52a271c06e41aad45b7b34b20
Pol Trujillo <pol.trujillo@sky.uk>
2026-03-26 11:04:03 +0000

    Exercice 1

--- src/customer/CustomerList.spec.js
+++ src/customer/CustomerList.spec.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { useContext } from 'react';
 import useAlert from './hooks/useAlert';
+import CustomerList from "./CustomerList";
+import {screen, render} from "@testing-library/react";
+import {CustomerContext} from "./CustomerContext";
 
 // Replaces useContext with a Jest mock function for controlled testing
 jest.mock('react', () => ({
@@ -9,7 +12,7 @@ jest.mock('react', () => ({
 }));
 
 // Hint 1: Mock useAlert using jest.mock(.....), so we can control its return value in tests
-
+jest.mock('./hooks/useAlert.js');
 
 const customers = [
   {
@@ -20,14 +23,15 @@ const customers = [
   }
 ]
 const setHomeAlertText = jest.fn();
+const setHomeAlertVisible = jest.fn();
+
 // Hint 2: create another mock function, for setHomeAlertVisible
 
 describe('CustomerList', () => {
   beforeEach(() => {
     // Mocks the return value of useContext
     useContext.mockReturnValue({ customers });
-
-    // TODO: Mock the return value of useAlert
+    useAlert.mockReturnValue(true);
   });
 
   // Clears mock call/instance history after every test, to avoid tests affecting each-other
@@ -37,15 +41,32 @@ describe('CustomerList', () => {
 
   //TODO: these tests must be changed from it.todo('{test-name}') to it('{test-name}', () => { ... }
   describe('Client added alert', () => {
-    it.todo('should render an alert with text New client has been added when displayAlert from hook is true');
-    // 1. Render the component
-    // 2. Check that the hooks have been called
-    // 3. Check that the alert is displayed (hint: check for the text in the alert)
-
-    it.todo('should not render an alert with text New client has been added when displayAlert from hook is false');
+    it('should render an alert with text New client has been added when displayAlert from hook is true', () => {
+      render(<CustomerList setHomeAlertVisible={setHomeAlertVisible} setHomeAlertText={setHomeAlertText} />);
+      expect(screen.getByText("New client has been added")).toBeInTheDocument();
+      expect(useAlert).toHaveBeenCalledWith();
+      expect(useAlert).toHaveBeenCalledTimes(1);
+      expect(useContext).toHaveBeenCalledWith(CustomerContext);
+      expect(useContext).toHaveBeenCalledTimes(1);
+    });
+    it('should not render an alert with text New client has been added when displayAlert from hook is false', () => {
+      useAlert.mockReturnValue(false);
+      render(<CustomerList setHomeAlertVisible={setHomeAlertVisible} setHomeAlertText={setHomeAlertText} />);
+      expect(screen.queryByText("New client has been added")).not.toBeInTheDocument();
+      expect(useAlert).toHaveBeenCalledWith();
+      expect(useAlert).toHaveBeenCalledTimes(1);
+      expect(useContext).toHaveBeenCalledWith(CustomerContext);
+      expect(useContext).toHaveBeenCalledTimes(1);
+    });
   });
 
   describe('Display client list', () => {
-    it.todo('should display context clients');
+    it('should display context clients', () => {
+      render(<CustomerList setHomeAlertVisible={setHomeAlertVisible} setHomeAlertText={setHomeAlertText} />);
+      expect(screen.getByText(/bob@sky.uk/i)).toBeInTheDocument();
+      expect(screen.getByText(/77777777777/i)).toBeInTheDocument();
+      expect(screen.getByText(/Watermark/i)).toBeInTheDocument();
+      expect(screen.getByText("Bob")).toBeInTheDocument();
+    });
   });
 });
